4d6d64aa58d0d268c440cc9f8e35575fdca09d85
Alexis-NM <alexis.na.me@gmail.com>
2023-11-28 12:45:11 +0100

    users post on db

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -45,20 +45,37 @@ const getMovieById = (req, res) => {
   database
     .query("select * from movies where id = ?", [id])
     .then(([movies]) => {
-    if (movies[0] != null) {
-    res.json(movies[0]);
-  } else {
-    res.status(404).send("Not Found");
-  }
-})
+      if (movies[0] != null) {
+        res.json(movies[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
     .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
+      console.error(err);
+      res.sendStatus(500);
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+    res.status(201).send({ id:result.insertId});   
+   })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
